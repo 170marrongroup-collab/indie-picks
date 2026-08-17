2a6a277d937466a29f6f9d150ff9bc924f9bfc2f
170marrongroup-collab <170.marrongroup@gmail.com>
2026-08-17 12:21:45 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
+      // FANZA
       {
         protocol: "https",
         hostname: "pics.dmm.co.jp",
@@ -11,6 +12,30 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "doujin-assets.dmm.co.jp",
+        pathname: "/**",
+      },
+
+      // Pcolle
+      {
+        protocol: "https",
+        hostname: "img.pcolle.com",
+        pathname: "/**",
+      },
+
+      // FC2
+      {
+        protocol: "https",
+        hostname: "contents-thumbnail2.fc2.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "contents-thumbnail1.fc2.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "contents-thumbnail3.fc2.com",
         pathname: "/**",
       },
     ],
